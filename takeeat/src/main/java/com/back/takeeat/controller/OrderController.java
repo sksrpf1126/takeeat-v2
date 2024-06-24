c3b02c1668bb4cf8db78abe5c84d9a26cd451ec7
GYUNGAEEEE <ko2931@naver.com>
2024-06-25 00:05:35 +0900

    (order)marketInfoTab-front

--- takeeat/src/main/java/com/back/takeeat/controller/OrderController.java
+++ takeeat/src/main/java/com/back/takeeat/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.back.takeeat.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/order")
 public class OrderController {
 
+    @Value("${KAKAO_API_KEY}")
+    String KAKAO_API_KEY;
+
     @GetMapping("/menuList")
-    public String menuList() {
+    public String menuList(Model model) {
+        model.addAttribute("KAKAO_API_KEY", KAKAO_API_KEY);
         return "order/marketMenu";
     }
 }
--- takeeat/src/main/resources/static/js/order/marketMenuScript.js
+++ takeeat/src/main/resources/static/js/order/marketMenuScript.js
@@ -1,3 +1,4 @@
+//탭 메뉴
 document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tabs input');
     const contents = document.querySelectorAll('.conbox');
@@ -19,3 +20,40 @@ document.addEventListener('DOMContentLoaded', function () {
     tabs.forEach(tab => tab.addEventListener('change', showContent));
     showContent();
 });
+
+//지도 api
+var map = null;
+
+window.onload = function() {
+    kakao.maps.load(function() {
+        var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+            mapOption = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };
+
+        map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+        // 마커가 표시될 위치입니다
+        var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+            position: markerPosition
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+    });
+}
+
+function showInfoTab() {
+    setTimeout(function() {
+        relayout();
+    }, 100); // 100ms 정도 지연을 추가하여 영역이 완전히 렌더링되도록 합니다.
+}
+
+function relayout() {
+    map.relayout();
+    map.setCenter(new kakao.maps.LatLng(33.450701, 126.570667));
+}

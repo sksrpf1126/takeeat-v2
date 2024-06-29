fa9fde732998156c7a08e70ed367ec272665be54
limseonghyun <sksrpf1126@naver.com>
2024-06-29 17:32:09 +0900

    feat: 회원 알림 및 가게 주문관리 페이지 추가

---
+++ takeeat/src/main/java/com/back/takeeat/controller/NotificationController.java
@@ -0,0 +1,21 @@
+package com.back.takeeat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notification")
+public class NotificationController {
+
+    @GetMapping("/member")
+    public String notificationMember() {
+        return "notification/notificationMember";
+    }
+
+    @GetMapping("/market")
+    public String marketOrder() {
+        return "notification/marketOrder";
+    }
+
+}

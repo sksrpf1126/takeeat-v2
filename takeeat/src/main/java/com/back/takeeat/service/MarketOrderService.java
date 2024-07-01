74ca6ee04317c783b0a5f9db07cbbe174f632fa0
limseonghyun <sksrpf1126@naver.com>
2024-07-01 15:39:49 +0900

    refactor: MarketOrder 데이터 정렬 검색으로 변경

--- takeeat/src/main/java/com/back/takeeat/service/MarketOrderService.java
+++ takeeat/src/main/java/com/back/takeeat/service/MarketOrderService.java
@@ -2,6 +2,7 @@ package com.back.takeeat.service;
 
 import com.back.takeeat.domain.order.Order;
 import com.back.takeeat.domain.order.OrderStatus;
+import com.back.takeeat.dto.marketorder.request.MarketOrderSearchRequest;
 import com.back.takeeat.dto.marketorder.response.MarketOrdersResponse;
 import com.back.takeeat.dto.marketorder.response.OrdersCountResponse;
 import com.back.takeeat.repository.MarketOrderRepository;
@@ -32,9 +33,9 @@ public class MarketOrderService {
 
 
     @Transactional(readOnly = true)
-    public List<MarketOrdersResponse> getOrdersByStatusCondition(Long marketId, OrderStatus orderStatus) {
+    public List<MarketOrdersResponse> getOrdersByStatusCondition(Long marketId, MarketOrderSearchRequest searchRequest) {
 
-        List<Order> findOrders = marketOrderRepository.findAllWithOrderMenus(marketId, orderStatus);
+        List<Order> findOrders = marketOrderRepository.findAllWithSortOrder(marketId, searchRequest);
 
         return MarketOrdersResponse.listOf(findOrders);
     }

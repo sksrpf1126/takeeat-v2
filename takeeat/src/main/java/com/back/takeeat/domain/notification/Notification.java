2caf8696c5ecc8ed7ab8b77362aab6b2b4ee257c
limseonghyun <sksrpf1126@naver.com>
2024-07-02 19:21:31 +0900

    feat: Notification Entity createdTime Field Add

--- takeeat/src/main/java/com/back/takeeat/domain/notification/Notification.java
+++ takeeat/src/main/java/com/back/takeeat/domain/notification/Notification.java
@@ -2,8 +2,18 @@ package com.back.takeeat.domain.notification;
 
 import com.back.takeeat.domain.user.Member;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@EntityListeners(value = AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification {
 
     @Id
@@ -14,6 +24,9 @@ public class Notification {
     private String contents;
 
     private String watched;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")

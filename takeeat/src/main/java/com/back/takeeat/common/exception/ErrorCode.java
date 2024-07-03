b44eda103a9d7ff3aba63dcf58f2458c2e6e385d
limseonghyun <sksrpf1126@naver.com>
2024-07-03 15:19:26 +0900

    feat: Order 관련 ErrorCode 추가

--- takeeat/src/main/java/com/back/takeeat/common/exception/ErrorCode.java
+++ takeeat/src/main/java/com/back/takeeat/common/exception/ErrorCode.java
@@ -6,7 +6,9 @@ public enum ErrorCode {
 
     MARKET_NOT_FOUND(400, "M_001", "가게를 찾을 수 없습니다."),
 
-    ORDER_NOT_FOUND(400, "O_001", "주문을 찾을 수 없습니다.");
+    ORDER_NOT_FOUND(400, "O_001", "주문을 찾을 수 없습니다."),
+    ORDER_STATUS_MISMATCH(400, "O_003", "주문 상태가 일치하지 않습니다."),
+    ORDER_UNAUTHORIZED(403, "O_002", "해당 주문에 대한 권한이 없습니다.");
 
     private final String code;
     private final String message;

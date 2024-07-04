d0afb71c3e9dcc6938329e91f8ebf3ab871ea9a2
limseonghyun <sksrpf1126@naver.com>
2024-07-04 10:05:28 +0900

    feat: MENU_NOT_FOUND ErrorCode Add

--- takeeat/src/main/java/com/back/takeeat/common/exception/ErrorCode.java
+++ takeeat/src/main/java/com/back/takeeat/common/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
 
     ORDER_NOT_FOUND(400, "O_001", "주문을 찾을 수 없습니다."),
     ORDER_STATUS_MISMATCH(400, "O_003", "주문 상태가 일치하지 않습니다."),
-    ORDER_UNAUTHORIZED(403, "O_002", "해당 주문에 대한 권한이 없습니다.");
+    ORDER_UNAUTHORIZED(403, "O_002", "해당 주문에 대한 권한이 없습니다."),
+
+    MENU_NOT_FOUND(400, "MN_001", "메뉴를 찾을 수 없습니다.");
 
     private final String code;
     private final String message;

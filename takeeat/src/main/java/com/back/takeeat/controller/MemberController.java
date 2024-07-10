f83cd43b761f9fe2c637c5cd2f0f85dbac087279
limseonghyun <sksrpf1126@naver.com>
2024-07-10 17:14:49 +0900

    feat: 로그인, 약관 동의, 일반 회원가입, 소셜 회원가입 페이지 및 로직 추가

--- takeeat/src/main/java/com/back/takeeat/controller/MemberController.java
+++ takeeat/src/main/java/com/back/takeeat/controller/MemberController.java
@@ -1,13 +1,18 @@
 package com.back.takeeat.controller;
 
 import com.back.takeeat.common.exception.AuthException;
+import com.back.takeeat.common.exception.BaseException;
 import com.back.takeeat.common.exception.ErrorCode;
+import com.back.takeeat.common.exception.ErrorPageException;
+import com.back.takeeat.domain.user.Member;
 import com.back.takeeat.domain.user.ProviderType;
 import com.back.takeeat.dto.member.SignupRequest;
+import com.back.takeeat.security.LoginMember;
 import com.back.takeeat.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -17,13 +22,43 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    //@TODO 테스트 후 지울 것
+    @GetMapping("/test")
+    public String test(@RequestParam("errorType") String errorType) {
+
+        if(errorType.equals("api")) {
+            throw new BaseException(ErrorCode.MEMBER_EXISTS);
+        }else if(errorType.equals("page")) {
+            throw new ErrorPageException(ErrorCode.ORDER_UNAUTHORIZED);
+        }
+
+        return "member/loginForm";
+    }
+
     @GetMapping("/login")
-    public String memberLoginForm() {
+    public String memberLoginForm(
+            @LoginMember Member member,
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "exception", required = false) String exception,
+            Model model
+    ) {
+        if(member != null) {
+            //@TODO 메인페이지 경로로 변경할 것
+            return "redirect:/market/info";
+        }
+
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "member/loginForm";
     }
 
     @GetMapping("/policy-agreement")
-    public String memberPolicyAgreement(@RequestParam("loginType") String loginType, HttpServletRequest request) {
+    public String memberPolicyAgreement(@LoginMember Member member, @RequestParam(value = "loginType", required = false) String loginType, HttpServletRequest request) {
+        if(member != null) {
+            //@TODO 메인페이지 경로로 변경할 것
+            return "redirect:/market/info";
+        }
+
         if(loginType == null || !loginType.equals("social")) {
             request.getSession().removeAttribute("email");
             request.getSession().removeAttribute("providerType");
@@ -32,12 +67,22 @@ public class MemberController {
     }
 
     @GetMapping("/social-signup")
-    public String socialSignupForm() {
+    public String socialSignupForm(@LoginMember Member member) {
+        if(member != null) {
+            //@TODO 메인페이지 경로로 변경할 것
+            return "redirect:/market/info";
+        }
+
         return "member/socialSignup";
     }
 
     @GetMapping("/default-signup")
-    public String signupForm() {
+    public String signupForm(@LoginMember Member member) {
+        if(member != null) {
+            //@TODO 메인페이지 경로로 변경할 것
+            return "redirect:/market/info";
+        }
+
         return "member/signup";
     }
 

cb4dcab922291dd8388acea0f0da466adb9b32a4
limseonghyun <sksrpf1126@naver.com>
2024-08-31 16:20:03 +0900

    refactor: RedisService Method 명 수정

--- takeeat/src/main/java/com/back/takeeat/service/RedisService.java
+++ takeeat/src/main/java/com/back/takeeat/service/RedisService.java
@@ -12,15 +12,13 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
     private final RedisTemplate<String, String> redisTemplate;
 
-    //email을 key값 code를 value로 하여 3분동안 저장한다.
-    public void setCode(String email,String code){
+    public void setAuthCode(String email, String code){
         ValueOperations<String, String> valOperations = redisTemplate.opsForValue();
-        //만료기간 3분
-        valOperations.set(email,code,180, TimeUnit.SECONDS);
+        //만료기간 5분
+        valOperations.set(email,code,300, TimeUnit.SECONDS);
     }
 
-    //key값인 email에 있는 value를 가져온다.
-    public String getCode(String email){
+    public String getAuthCode(String email){
         ValueOperations<String, String> valOperations = redisTemplate.opsForValue();
         return valOperations.get(email);
     }
